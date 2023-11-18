cb4b3f3ce3e09b6ca47dd229e9e3523f5d648f92
lima2020 <lima2020@ejemplo.com>
2023-11-18 10:32:03 -0500

    segundo avance

---
+++ src/main/java/Controlador/ControladorAuto.java
@@ -0,0 +1,54 @@
+package Controlador;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Dao.ClassCrudAuto;
+import model.TblAuto;
+
+/**
+ * Servlet implementation class ControladorAuto
+ */
+public class ControladorAuto extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ControladorAuto() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//INSTANCIAMOS LA RESPECTIVA CLASE..
+		ClassCrudAuto cruauto=new ClassCrudAuto();
+		TblAuto tblaito=new TblAuto();
+		//enviamos a la vista 
+		List<TblAuto> listado=cruauto.Listado();
+		request.setAttribute("listado", listado);
+		//redireccionemos
+		request.getRequestDispatcher("/ListadoAutos.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
